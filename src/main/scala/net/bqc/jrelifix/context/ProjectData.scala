b3d2adb9968665fd71dd5c83ce6bb2cfbeb44391
bqcuong <bqcuong2212@gmail.com>
2020-02-20 17:22:42 +0700

    update

--- src/main/scala/net/bqc/jrelifix/context/ProjectData.scala
+++ src/main/scala/net/bqc/jrelifix/context/ProjectData.scala
@@ -8,14 +8,15 @@ import net.bqc.jrelifix.identifier.Identifier
 import net.bqc.jrelifix.utils.ASTUtils
 import org.eclipse.jdt.core.dom.{ASTNode, CompilationUnit}
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 case class ProjectData() {
-  val compilationUnitMap: scala.collection.mutable.HashMap[String, CompilationUnit] = new scala.collection.mutable.HashMap[String, CompilationUnit]
-  val class2FilePathMap: scala.collection.mutable.HashMap[String, String] = new scala.collection.mutable.HashMap[String, String]
+  val compilationUnitMap: mutable.HashMap[String, CompilationUnit] = new mutable.HashMap[String, CompilationUnit]
+  val class2FilePathMap: mutable.HashMap[String, String] = new mutable.HashMap[String, String]
   val sourceFilesArray: ArrayBuffer[String] = ArrayBuffer[String]()
   val sourceFileContents: java.util.HashMap[String, DocumentASTRewrite] = new util.HashMap[String, DocumentASTRewrite]()
-  val changedSourcesMap: scala.collection.mutable.HashMap[String, ChangedFile] = new scala.collection.mutable.HashMap[String, ChangedFile]
+  val changedSourcesMap: mutable.HashMap[String, ChangedFile] = new mutable.HashMap[String, ChangedFile]
 
   def class2CU(className: String): CompilationUnit = compilationUnitMap(class2FilePathMap(className))
   def filePath2CU(relativeFilePath: String): CompilationUnit = compilationUnitMap(relativeFilePath)
--- src/main/scala/net/bqc/jrelifix/context/mutation/DeleteMutation.scala
+++ src/main/scala/net/bqc/jrelifix/context/mutation/DeleteMutation.scala
@@ -2,19 +2,22 @@ package net.bqc.jrelifix.context.mutation
 
 import net.bqc.jrelifix.context.ProjectData
 import net.bqc.jrelifix.identifier.Identifier
-import net.bqc.jrelifix.utils.ASTUtils
+import net.bqc.jrelifix.utils.{ASTUtils, DiffUtils}
 import org.eclipse.text.edits.TextEdit
 
 case class DeleteMutation(faultStatement: Identifier, projectData: ProjectData)
   extends Mutation(faultStatement, projectData) {
 
   override def mutate(): Unit = {
-    // Modify source code on ASTRewrite
-    ASTUtils.removeNode(this.astRewrite, faultStatement.getJavaNode())
+    // delete only when the fault line is modified in examining commit
+    if (DiffUtils.isChanged(projectData.changedSourcesMap, faultStatement)) {
+      // Modify source code on ASTRewrite
+      ASTUtils.removeNode(this.astRewrite, faultStatement.getJavaNode())
 
-    // Apply changes to the document object
-    val edits = this.astRewrite.rewriteAST(this.document.modifiedDocument, null)
-    edits.apply(this.document.modifiedDocument, TextEdit.NONE)
+      // Apply changes to the document object
+      val edits = this.astRewrite.rewriteAST(this.document.modifiedDocument, null)
+      edits.apply(this.document.modifiedDocument, TextEdit.NONE)
+    }
   }
 
   override def unmutate(): Unit = ???
