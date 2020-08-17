6d3b09c578f021c5d450de5fcf4f5d6d62566cc1
bqcuong <bqcuong2212@gmail.com>
2020-08-17 23:25:26 +0700

    update

--- src/main/scala/net/bqc/lyfix/Main.scala
+++ src/main/scala/net/bqc/lyfix/Main.scala
@@ -35,7 +35,8 @@ object Main {
 //    projectData.bugId = "tananaev-traccar-68883949"
 //    projectData.bugId = "tananaev-traccar-82839755"
 //    projectData.bugId = "Bears-251"
-    projectData.bugId = "sannies-mp4parser-79111320"
+//    projectData.bugId = "sannies-mp4parser-79111320"
+    projectData.bugId = "stagemonitor-stagemonitor-145477129"
 
 //    configLog4J(projectData.bugId)
     val predefinedArgs = FileFolderUtils.readFile("ArgFiles/%s.txt".format(projectData.bugId))
