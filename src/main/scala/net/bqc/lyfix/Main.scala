6e5ab1b618843d553372a6bb0304a7b63dfd815d
bqcuong <bqcuong2212@gmail.com>
2020-08-17 18:06:16 +0700

    Bears-127

--- src/main/scala/net/bqc/lyfix/Main.scala
+++ src/main/scala/net/bqc/lyfix/Main.scala
@@ -30,6 +30,7 @@ object Main {
   def main(args: Array[String]): Unit = {
 //    projectData.bugId = "Bears-139"
     projectData.bugId = "Bears-127"
+//    projectData.bugId = "Bears-98"
 
 //    configLog4J(projectData.bugId)
     val predefinedArgs = FileFolderUtils.readFile("ArgFiles/%s.txt".format(projectData.bugId))
