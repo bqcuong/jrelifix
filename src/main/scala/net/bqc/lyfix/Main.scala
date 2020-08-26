389057f0dcdc698fa5e2288c568857eac903ad07
bqcuong <bqcuong2212@gmail.com>
2020-08-26 16:47:05 +0700

    update

--- src/main/scala/net/bqc/lyfix/Main.scala
+++ src/main/scala/net/bqc/lyfix/Main.scala
@@ -61,6 +61,7 @@ object Main {
     projectData.bugId = "Bears-4"
     projectData.bugId = "Bears-21"
     projectData.bugId = "Bears-3"
+    projectData.bugId = "Bears-25"
 
 //    configLog4J(projectData.bugId)
     val predefinedArgs = FileFolderUtils.readFile("ArgFiles/%s.txt".format(projectData.bugId))
